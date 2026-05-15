60d2e1de6eb9511e67cf1db42ac70548d42f9c82
Rens <r.dewit1@students.uu.nl>
2026-05-15 12:54:54 +0200

    Add ChatGPT implementation of random dendrogram generation.

--- dendogram_generator.py
+++ dendogram_generator.py
@@ -1,18 +1,67 @@
-# this program generates a Random Hierarchical Graph (HRG) using the pyhrg library, where
-# the dendrogram is 'valid', i.e. each internal node has a probability that is at least its parent's probability
-
 from pyhrg.hrg import Dendrogram
-from random import shuffle
+
+import random as rnd
 import networkx as nx
 
-# Given number of vertices n, make a completely random hierarchical random graph
-def make_rnd_dendrogram(n: int) -> Dendrogram:
-    # initialize a dummy graph with n vertices and no edges.
-    dummy_graph = nx.Graph()
-    dummy_graph.add_nodes_from(range(0, n))
 
-    # make a completely random hierarchical random graph with n graph nodes
-    dendrogram: Dendrogram = Dendrogram(dummy_graph)
+def generate_probs(n: int) -> list[float]:
+    """
+    Generate n random probabilities sorted ascending.
+    """
+    probs = [rnd.uniform(0.0, 1.0) for _ in range(n)]
+    probs.sort()
+    return probs
+
+
+def assign_probs_topdown(
+    dendrogram: Dendrogram,
+    probs: list[float],
+    node="_D0",
+    index=0
+) -> int:
+    """
+    Assign probabilities recursively in preorder traversal.
+
+    Parents are assigned before children, guaranteeing:
+
+        child.p >= parent.p
+    """
+
+    dendrogram.nodes[node]["p"] = probs[index]
+    index += 1
+
+    children = dendrogram.children(node)
+
+    for child in children:
+        if dendrogram.is_dendrogram_node(child):
+            index = assign_probs_topdown(
+                dendrogram,
+                probs,
+                child,
+                index
+            )
+
+    return index
+
+
+def make_rnd_dendrogram(n: int) -> Dendrogram:
+
+    # dummy graph with n vertices
+    dummy_graph = nx.Graph()
+    dummy_graph.add_nodes_from(range(n))
+
+    # build random dendrogram structure
+    dendrogram = Dendrogram(dummy_graph)
     dendrogram.initialize()
+
+    # number of internal dendrogram nodes
+    dnodes = list(dendrogram.dendrogram_nodes())
+    num_dnodes = len(dnodes)
+
+    # generate distributed probabilities
+    probs = generate_probs(num_dnodes)
+
+    # assign them respecting hierarchy
+    assign_probs_topdown(dendrogram, probs)
 
     return dendrogram
