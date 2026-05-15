2aaf781639006afccb30332ad93d7b87ab3a80dd
Rens <r.dewit1@students.uu.nl>
2026-05-15 12:42:16 +0200

    Make initial make_rnd_dendrogram function, which does not have the probability restriction yet.

--- dendogram_generator.py
+++ dendogram_generator.py
@@ -1,1 +1,18 @@
-# this program takes a real network and gives back a dendogram (based on the paper metho)
+# this program generates a Random Hierarchical Graph (HRG) using the pyhrg library, where
+# the dendrogram is 'valid', i.e. each internal node has a probability that is at least its parent's probability
+
+from pyhrg.hrg import Dendrogram
+from random import shuffle
+import networkx as nx
+
+# Given number of vertices n, make a completely random hierarchical random graph
+def make_rnd_dendrogram(n: int) -> Dendrogram:
+    # initialize a dummy graph with n vertices and no edges.
+    dummy_graph = nx.Graph()
+    dummy_graph.add_nodes_from(range(0, n))
+
+    # make a completely random hierarchical random graph with n graph nodes
+    dendrogram: Dendrogram = Dendrogram(dummy_graph)
+    dendrogram.initialize()
+
+    return dendrogram
